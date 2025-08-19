c1b674b9c413fe4bf755743f07bc68217356232a
vaibhavi-parmar <vaibhaviparmar70@gmail.com>
2025-08-19 11:17:00 +0530

    Admin panel changes 3

--- vendor/admin.py
+++ vendor/admin.py
@@ -3,11 +3,6 @@ from .models import *
 
 # Register your models here.
 
-admin.site.site_title = "Admin"
-admin.site.site_header = "Diamond System"
-admin.site.index_title = "Diamond System Administration"
-admin.site.site_url = '/'
-
 admin.site.register(Vendor)
 admin.site.register(Diamond)
 admin.site.register(Order)
