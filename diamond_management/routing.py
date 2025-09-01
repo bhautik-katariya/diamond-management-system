12102b3c51e2e8fb4552568a0a8ddcd3469420c3
vaibhavi-parmar <vaibhaviparmar70@gmail.com>
2025-09-01 17:45:52 +0530

    minor corrections

--- diamond_management/routing.py
+++ diamond_management/routing.py
@@ -1,6 +1,5 @@
 from django.urls import re_path
 from vendor import consumers
-from customer import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/vendor/orders/(?P<vendor_id>\d+)/$", consumers.OrderConsumer.as_asgi()),
